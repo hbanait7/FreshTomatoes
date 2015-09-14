d9ae99b896cf78e78350f15b339ab160b6114f5c
hbanait <hbanait7@gmail.com>
2015-09-14 12:35:13 -0400

    Finish Movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,60 @@
 class MoviesController < ApplicationController
+
+  before_filter :set_movie, only: [:show, :edit, :update, :destroy]
+
   def index
+    @movies = Movie.all
   end
 
   def show
+
   end
 
   def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      flash[:notice] = 'Movie Successfully Created'
+      redirect_to movie_path(@movie)
+    else
+      flash.now[:alert] = 'Invalid Movie Form'
+      render 'new'
+    end
   end
 
   def edit
+
   end
+
+  def update
+    if @movie.update_attributes(movie_params)
+      flash[:notice] = 'Movie Successfully Updated'
+      redirect_to movie_path(@movie)
+    else
+      flash.now[:alert] = 'Invalid Movie Form'
+      render 'edit'
+    end      
+  end
+
+  def destroy
+    @movie.destroy
+    flash[:notice] = 'Movie Successfully Deleted'
+    redirect_to root_path
+  end
+
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_date, :genre, :description, :director, :actors)
+  end
+
 end
